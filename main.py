4116746067e6ca6cdb742f02b2b81f4f525db11f
Anatoliy <zhuravvlikk44@gmail.com>
2026-02-27 13:58:30 +0300

    Add mimi function

--- main.py
+++ main.py
@@ -1,6 +1,160 @@
 from datetime import datetime
 import json
 
+def add_income():
+    global balance, operations
+    amount = int(input('Сумма дохода:'))
+    print('Выберите категорию дохода:')
+    for i, cat in enumerate(income_categories, 1):
+        print(f'{i}. {cat}')
+    print(f"{len(income_categories) + 1}. Другая (ввести вручную)")
+
+    cat_choice = int(input('Ваш выбор:'))
+
+    if 1 <= cat_choice <= len(income_categories):
+        category = income_categories[cat_choice - 1]
+    else:
+        category = input('Введите название категории:')
+
+    operations.append({
+        'type': 'доход',
+        'amount': amount,
+        'category': category,
+        'date': datetime.now().strftime('%d.%m.%Y %H:%M')
+    })
+    balance += amount
+    print(f'Добавлен доход: {amount} руб.')
+    save_operations()
+
+def add_expense():
+    global balance, operations
+    amount = int(input('Сумма расхода:'))
+    print('Выберите категорию расхода:')
+    for i, cat in enumerate(expense_categories, 1):
+        print(f'{i}. {cat}')
+    print(f"{len(expense_categories) + 1}. Другая (ввести вручную)")
+
+    cat_choice = int(input('Ваш выбор:'))
+
+    if 1 <= cat_choice <= len(expense_categories):
+        category = expense_categories[cat_choice - 1]
+    else:
+        category = input('Введите название категории:')
+
+    operations.append({
+        'type': 'расход',
+        'amount': amount,
+        'category': category,
+        'date': datetime.now().strftime('%d.%m.%Y %H:%M')
+    })
+    balance -= amount
+    print(f'Добавлен расход: {amount} руб.')
+    save_operations()
+
+def history_operations():
+    print('\n====== ИСТОРИЯ ОПЕРАЦИЙ =======')
+
+    if not operations:
+        print('История пуста.')
+    else:
+
+        for operation in operations:
+            print(f"{operation['date']} | {operation['type']:6} | {operation['category']:10}"
+                  f" | {operation['amount']:>8} руб.")
+        print('===========\n')
+
+
+def edit_operations():
+    global operations, balance
+    print('Редактирование операции')
+    while True:
+        if not show_operations_with_number():
+            continue
+        try:
+            op_num = int(input('Введите номер операции для редактирования: '))
+            if 1 <= op_num <= len(operations):
+                op = operations[op_num - 1]
+                old_amount = op['amount']
+                old_type = op['type']
+                print(f"\nТекущая операция: {op['date']} | {op['type']} | {op['category']} | {op['amount']} руб.")
+                print('Что редактируем? ')
+                print('1. Сумму')
+                print('2. Категорию')
+                print('3. Всё')
+                edit_choice = int(input('Ваш выбор:'))
+
+                if op['type'] == 'доход':
+                    balance -= op['amount']
+                else:
+                    balance += op['amount']
+
+                if edit_choice in [1, 3]:
+                    new_amount = int(input('Новая сумма: '))
+                    op['amount'] = new_amount
+
+                    if op['type'] == 'доход':
+                        balance += new_amount
+                    else:
+                        balance -= new_amount
+
+                if edit_choice in [2, 3]:
+                    print('Выберите новую категорию: ')
+                    if op['type'] == 'доход':
+                        categories = income_categories
+                    else:
+                        categories = expense_categories
+
+                    for i, cat in enumerate(categories, 1):
+                        print(f'{i}. {cat}')
+                    print(f'{len(categories) + 1}. ✏️ Другая (ввести вручную)')
+                    try:
+                        cat_choice = int(input('Ваш выбор: '))
+                    except ValueError:
+                        print('Введите число!')
+                    if 1 <= cat_choice <= len(categories):
+                        op['category'] = categories[cat_choice - 1]
+                    else:
+                        op['category'] = input('Введите название категории: ')
+
+                op['date'] = datetime.now().strftime('%d.%m.%Y %H:%M')
+
+                save_operations()
+                print('Операция успешно отредактирована!')
+                break
+            else:
+                print('Неверный номер операции!')
+        except ValueError:
+            print('Пожалуйста, введите число!')
+
+def del_operations():
+    global operations, balance
+    print('Удаление операции')
+    while True:
+        if not show_operations_with_number():
+            continue
+        try:
+            op_num = int(input('Введите номер операции для удаления: '))
+            if 1 <= op_num <= len(operations):
+                op = operations[op_num - 1]
+                print('Операция для удаления: ')
+                print(f"{op['date']} | {op['type']} | {op['category']} | {op['amount']} руб.")
+
+                confirm = input('Подтвердить удаление? (да/нет)')
+                if confirm.lower() == 'да':
+                    if op['type'] == 'доход':
+                        balance -= op['amount']
+                    else:
+                        balance += op['amount']
+                    del operations[op_num - 1]
+                    save_operations()
+                    print('Операция удалена!')
+                else:
+                    print('Удаление отменено!')
+            else:
+                print('Номер операции неверный!')
+        except ValueError:
+            print('Пожалуйста, введите число!')
+
 def save_operations():
     global operations, balance
     with open('finance_data.json', 'w', encoding='utf-8') as file:
@@ -96,153 +250,19 @@ while True:
         choice = int(input('\nВыберите действие:'))
 
         if choice == 1:
-            amount = int(input('Сумма дохода:'))
-            print('Выберите категорию дохода:')
-            for i, cat in enumerate(income_categories, 1):
-                print(f'{i}. {cat}')
-            print(f"{len(income_categories)+1}. Другая (ввести вручную)")
-
-            cat_choice = int(input('Ваш выбор:'))
-
-            if 1 <= cat_choice <= len(income_categories):
-                category = income_categories[cat_choice - 1]
-            else:
-                category = input('Введите название категории:')
-
-
-            operations.append({
-                'type': 'доход',
-                'amount': amount,
-                'category': category,
-                'date': datetime.now().strftime('%d.%m.%Y %H:%M')
-            })
-            balance += amount
-            print(f'Добавлен доход: {amount} руб.')
-            save_operations()
+            add_income()
 
         elif choice == 2:
-            amount = int(input('Сумма расхода:'))
-            print('Выберите категорию расхода:')
-            for i, cat in enumerate(expense_categories, 1):
-                print(f'{i}. {cat}')
-            print(f"{len(expense_categories) + 1}. Другая (ввести вручную)")
-
-            cat_choice = int(input('Ваш выбор:'))
-
-            if 1 <= cat_choice <= len(expense_categories):
-                category = expense_categories[cat_choice - 1]
-            else:
-                category = input('Введите название категории:')
-
-
-            operations.append({
-                'type': 'расход',
-                'amount': amount,
-                'category': category,
-                'date': datetime.now().strftime('%d.%m.%Y %H:%M')
-            })
-            balance -= amount
-            print(f'Добавлен расход: {amount} руб.')
-            save_operations()
+            add_expense()
 
         elif choice == 3:
-            print('\n====== ИСТОРИЯ ОПЕРАЦИЙ =======')
-
-            if not operations:
-                print('История пуста.')
-            else:
-
-                for operation in operations:
-                    print(f"{operation['date']} | {operation['type']:6} | {operation['category']:10}"
-                          f" | {operation['amount']:>8} руб.")
-                print('===========\n')
-
+            history_operations()
 
         elif choice == 4:
-            print('Редактирование операции')
-            if not show_operations_with_number():
-                continue
-            try:
-                op_num = int(input('Введите номер операции для редактирования: '))
-                if 1 <= op_num <= len(operations):
-                    op = operations[op_num - 1]
-                    old_amount = op['amount']
-                    old_type = op['type']
-                    print(f"\nТекущая операция: {op['date']} | {op['type']} | {op['category']} | {op['amount']} руб.")
-                    print('Что редактируем? ')
-                    print('1. Сумму')
-                    print('2. Категорию')
-                    print('3. Всё')
-                    edit_choice = int(input('Ваш выбор:'))
-
-                    if op['type'] == 'доход':
-                        balance -= op['amount']
-                    else:
-                        balance += op['amount']
-
-                    if edit_choice in [1,3]:
-                        new_amount = int(input('Новая сумма: '))
-                        op['amount'] = new_amount
-
-                        if op['type'] == 'доход':
-                            balance += new_amount
-                        else:
-                            balance -= new_amount
-
-                    if edit_choice in [2,3]:
-                        print('Выберите новую категорию: ')
-                        if op['type'] == 'доход':
-                            categories = income_categories
-                        else:
-                            categories = expense_categories
-
-                        for i, cat in enumerate(categories, 1):
-                            print(f'{i}. {cat}')
-                        print(f'{len(categories) + 1}. ✏️ Другая (ввести вручную)')
-                        try:
-                            cat_choice = int(input('Ваш выбор: '))
-                        except ValueError:
-                            print('Введите число!')
-                        if 1 <= cat_choice <= len(categories):
-                            op['category'] = categories[cat_choice - 1]
-                        else:
-                            op['category'] = input('Введите название категории: ')
-
-                    op['date'] = datetime.now().strftime('%d.%m.%Y %H:%M')
-
-                    save_operations()
-                    print('Операция успешно отредактирована!')
-                else:
-                    print('Неверный номер операции!')
-            except ValueError:
-                print('Пожалуйста, введите число!')
+            edit_operations()
 
         elif choice == 5:
-            print('Удаление операции')
-            if not show_operations_with_number():
-                continue
-            try:
-                op_num = int(input('Введите номер операции для удаления: '))
-                if 1 <= op_num <= len(operations):
-                    op = operations[op_num - 1]
-                    print('Операция для удаления: ')
-                    print(f"{op['date']} | {op['type']} | {op['category']} | {op['amount']} руб.")
-
-                    confirm = input('Подтвердить удаление? (да/нет)')
-                    if confirm.lower() == 'да':
-                        if op['type'] == 'доход':
-                            balance -= op['amount']
-                        else:
-                            balance += op['amount']
-                        del operations[op_num - 1]
-                        save_operations()
-                        print('Операция удалена!')
-                    else:
-                        print('Удаление отменено!')
-                else:
-                    print('Номер операции неверный!')
-            except ValueError:
-                print('Пожалуйста, введите число!')
+            del_operations()
 
         elif choice == 6:
             show_statistics()
